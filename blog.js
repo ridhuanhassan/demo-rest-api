e077dc2907db8287533a52cbc96bca591e2cd0eb
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2021-04-02 08:18:50 +0800

    welcome to mini-blog

---
+++ blog.js
@@ -0,0 +1,45 @@
+/**
+
+  ███╗░░░███╗██╗███╗░░██╗██╗░░░░░░██████╗░██╗░░░░░░█████╗░░██████╗░
+  ████╗░████║██║████╗░██║██║░░░░░░██╔══██╗██║░░░░░██╔══██╗██╔════╝░
+  ██╔████╔██║██║██╔██╗██║██║█████╗██████╦╝██║░░░░░██║░░██║██║░░██╗░
+  ██║╚██╔╝██║██║██║╚████║██║╚════╝██╔══██╗██║░░░░░██║░░██║██║░░╚██╗
+  ██║░╚═╝░██║██║██║░╚███║██║░░░░░░██████╦╝███████╗╚█████╔╝╚██████╔╝
+  ╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░░░░░╚═════╝░╚══════╝░╚════╝░░╚═════╝░
+
+  A blogging tool for a loner.
+
+  e-mail: ridhuan@leetcat.com
+ */
+
+const { logger } = require('./libs/helper');
+
+// Init Router
+require('./router').init();
+
+// Init HTTPS server
+const server = require('./libs/server').init();
+
+// Shutdown process gracefully
+function gracefulShutdown(signal) {
+  logger.warn(`${signal} signal received`);
+  logger.warn('Closing HTTPS server');
+
+  server.close(() => {
+    process.exit(0);
+  });
+
+  // if poll empty. emit close
+  // https://nodejs.org/en/docs/guides/event-loop-timers-and-nexttick/#poll
+  setImmediate(() => {
+    server.emit('close');
+  });
+}
+
+process.on('SIGINT', () => {
+  gracefulShutdown('SIGINT');
+});
+
+process.on('SIGTERM', () => {
+  gracefulShutdown('SIGTERM');
+});
