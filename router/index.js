e26c6b678b0c40acd94f9b31e5acdecaf1cf4654
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2021-04-03 22:04:49 +0800

    demo get /

--- router/index.js
+++ router/index.js
@@ -1,3 +1,15 @@
+const router = require('../libs/router');
+
 exports.init = () => {
+  router.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.write(JSON.stringify({
+      status: 200,
+      message: 'Demo REST API',
+    }));
+    res.end();
+  });
+
   // import routers
 };
