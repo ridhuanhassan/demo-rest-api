804c654fd91f8de0371c8c0989ac76c1610282aa
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2021-04-04 09:13:55 +0800

    first commit demo2 router

---
+++ router/demo2.js
@@ -0,0 +1,37 @@
+const router = require('../libs/router');
+const cat = require('../controllers/cat');
+const helper = require('../libs/helper');
+
+const simple = Symbol.for('simple');
+
+router.get('/cat/breeds', (req, res) => {
+  cat.listBreeds()
+    .then((breeds) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+      res.write(JSON.stringify(breeds));
+      res.end();
+    })
+    .catch((error) => {
+      helper.sendErrorResponse(res, error.message, 400);
+    });
+});
+
+router.get('/cat/breed/:breedId/images', (req, res) => {
+  const input = {
+    breedId: req[simple].params?.breedId,
+    limit: req[simple].query?.limit,
+    page: req[simple].query?.page,
+  };
+
+  cat.imageByBreed(input)
+    .then((images) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+      res.write(JSON.stringify(images));
+      res.end();
+    })
+    .catch((error) => {
+      helper.sendErrorResponse(res, error.message, 400);
+    });
+});
