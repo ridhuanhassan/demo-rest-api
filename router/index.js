3e013d714174dfdaafabeb16e9b366092b30adb3
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2021-04-04 09:14:19 +0800

    require from demo1 and demo2

--- router/index.js
+++ router/index.js
@@ -1,15 +1,5 @@
-const router = require('../libs/router');
-
 exports.init = () => {
-  router.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
-    res.write(JSON.stringify({
-      status: 200,
-      message: 'Demo REST API',
-    }));
-    res.end();
-  });
-
   // import routers
+  require('./demo1');
+  require('./demo2');
 };
