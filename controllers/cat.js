60b7d96727c79d23656d8a71a8e964021d49fe89
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2021-04-05 18:48:45 +0800

    update error handling and pagination, add delete image

--- controllers/cat.js
+++ controllers/cat.js
@@ -1,3 +1,4 @@
+// https://docs.thecatapi.com/api-reference
 const https = require('https');
 const { randomBytes } = require('crypto');
 
@@ -7,11 +8,14 @@ const cat = {};
 
 const apiKey = process.env.CAT_KEY || '582af1e5-ae1d-4410-a0bd-81b167ebebcd';
 
-cat.listBreeds = () => {
+cat.listBreeds = (input) => {
+  const page = input.page || 0;
+  const limit = input.limit || 10;
+
   const options = {
     hostname: 'api.thecatapi.com',
     port: 443,
-    path: '/v1/breeds',
+    path: `/v1/breeds?limit=${limit}&page=${page ? page - 1 : 0}`,
     method: 'GET',
     headers: {
       'X-Api-Key': apiKey,
@@ -33,24 +37,20 @@ cat.listBreeds = () => {
         const jsonified = JSON.parse(result);
 
         if (Math.floor(statusCode / 100) === 2 && contentType === 'application/json') {
-          if (Array.isArray(jsonified)) {
-            const catBreeds = [];
+          const catBreeds = [];
 
-            let i = 0;
-            const iMax = jsonified.length;
-            for (; i < iMax; i += 1) {
-              catBreeds.push({
-                id: jsonified[i]?.id,
-                name: jsonified[i]?.name,
-                image: jsonified[i]?.image?.url || 'n/a',
-              });
-            }
-
-            resolve(catBreeds);
-          } else {
-            reject(new Error('Expecting array'));
+          let i = 0;
+          const iMax = jsonified.length;
+          for (; i < iMax; i += 1) {
+            catBreeds.push({
+              id: jsonified[i]?.id,
+              name: jsonified[i]?.name,
+              image: jsonified[i]?.image?.url || 'n/a',
+            });
           }
 
+          resolve(catBreeds);
+        } else {
           reject(new Error(jsonified.message));
         }
       });
@@ -66,13 +66,13 @@ cat.listBreeds = () => {
 
 cat.imageByBreed = (input) => {
   const breedId = input.breedId;
-  const page = input.page || 1;
+  const page = input.page || 0;
   const limit = input.limit || 10;
 
   const options = {
     hostname: 'api.thecatapi.com',
     port: 443,
-    path: `/v1/images/search?limit=${limit}&page=${page}&breed_id=${breedId}`,
+    path: `/v1/images/search?limit=${limit}&page=${page ? page - 1 : 0}&breed_id=${breedId}`,
     method: 'GET',
     headers: {
       'X-Api-Key': apiKey,
@@ -94,25 +94,21 @@ cat.imageByBreed = (input) => {
         const jsonified = JSON.parse(result);
 
         if (Math.floor(statusCode / 100) === 2 && contentType === 'application/json') {
-          if (Array.isArray(jsonified)) {
-            const images = [];
+          const images = [];
 
-            let i = 0;
-            const iMax = jsonified.length;
-            for (; i < iMax; i += 1) {
-              images.push({
-                id: jsonified[i]?.id,
-                image: jsonified[i]?.url,
-              });
-            }
-
-            resolve(images);
-          } else {
-            reject(new Error('Expecting array'));
+          let i = 0;
+          const iMax = jsonified.length;
+          for (; i < iMax; i += 1) {
+            images.push({
+              id: jsonified[i]?.id,
+              image: jsonified[i]?.url,
+            });
           }
-        }
 
-        reject(new Error(jsonified.message));
+          resolve(images);
+        } else {
+          reject(new Error(jsonified.message));
+        }
       });
     });
 
@@ -184,9 +180,9 @@ cat.uploadImage = (input) => {
               id: jsonified?.id,
               image: jsonified?.url,
             });
+          } else {
+            reject(new Error(jsonified.message));
           }
-
-          reject(new Error(jsonified.message));
         });
       });
 
@@ -201,13 +197,13 @@ cat.uploadImage = (input) => {
 };
 
 cat.getUploadedImages = (input) => {
-  const page = input.page || 1;
+  const page = input.page || 0;
   const limit = input.limit || 10;
 
   const options = {
     hostname: 'api.thecatapi.com',
     port: 443,
-    path: `/v1/images?limit=${limit}&page=${page}`,
+    path: `/v1/images?limit=${limit}&page=${page ? page - 1 : 0}`,
     method: 'GET',
     headers: {
       'X-Api-Key': apiKey,
@@ -226,9 +222,9 @@ cat.getUploadedImages = (input) => {
         const statusCode = res.statusCode;
         const contentType = res.headers['content-type'].split(';')[0].trim();
 
-        if (statusCode === 200 && contentType === 'application/json') {
-          const jsonified = JSON.parse(result);
+        const jsonified = JSON.parse(result);
 
+        if (Math.floor(statusCode / 100) === 2 && contentType === 'application/json') {
           const images = [];
 
           let i = 0;
@@ -241,8 +237,54 @@ cat.getUploadedImages = (input) => {
           }
 
           resolve(images);
+        } else {
+          reject(new Error(jsonified.message));
+        }
+      });
+    });
 
-          reject(new Error('Unexpected result'));
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+};
+
+cat.deleteImage = (input) => {
+  const imageId = input.imageId;
+
+  const options = {
+    hostname: 'api.thecatapi.com',
+    port: 443,
+    path: `/v1/images/${imageId}`,
+    method: 'DELETE',
+    headers: {
+      'X-Api-Key': apiKey,
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let result = '';
+
+      res.on('data', (chunk) => {
+        result += chunk;
+      });
+
+      res.on('end', () => {
+        const statusCode = res.statusCode;
+
+        let jsonified;
+
+        if (result) {
+          jsonified = JSON.parse(result);
+        }
+
+        if (Math.floor(statusCode / 100) === 2) {
+          resolve();
+        } else {
+          reject(new Error(jsonified.message));
         }
       });
     });
