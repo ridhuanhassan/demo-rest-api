376cde41aae393a09e95ae078a29a087e3ee05b8
Ridhuan Hassan <mr.ridhuanhassan@gmail.com>
2021-04-06 16:36:52 +0800

    no need

--- routers/demo1.js
+++ routers/demo1.js
@@ -4,7 +4,6 @@ router.get('/', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   res.statusCode = 200;
   res.write(JSON.stringify({
-    status: 200,
     message: 'Demo REST API',
   }));
   res.end();
